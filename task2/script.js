6841200a24bf44a675a09d064d701162f6bd98eb
Aleksandr Alekseev (PHP-16) <mobacsell@yandex.ru>
2021-07-26 19:31:33 +0300

    add task2

---
+++ task2/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const   consoleLog = document.querySelector('#consoleLog'),
+        getAlertInfo = document.querySelector('#alert'),
+        getPromptInfo = document.querySelector('#prompt');
+
+consoleLog.addEventListener('click', () => {
+    alert('Метод console.log() позволяет вывыести сообщение в консоль');S
+});
+
+getAlertInfo.addEventListener('click', () => {
+    alert('Метод alert() выводит диалоговое окно с соответствующим сообщением');
+});
+
+getPromptInfo.addEventListener('click', () => {
+    alert('Метод prompt() выводит диалоговое окно с соответствующим сообщением и возможность ввода текста');
+});
